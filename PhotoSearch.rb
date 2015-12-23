bf7f845249c2983da46541445ec259d8aa676426
Vlad Chapaev <clearsky7free@gmail.com>
2015-12-23 15:36:00 +0200

    Add command line arguments

--- PhotoSearch.rb
+++ PhotoSearch.rb
@@ -1,30 +1,44 @@
+require 'optparse'
 require 'net/http'
 require 'json'
 
-alboms = ["14231802", "146106495", "216473097"]
+if ARGV.size >= 3
+  puts "Searching photos..."
+  personID = ARGV[0].to_s
+  groupID = ARGV[1].to_s
+  alboms = ARGV[2..ARGV.size]
 
-personID = "100"
-groupID = "-331509"
-photoList = Array.new
+  photoList = Array.new
+  for albom in alboms
+    offset = 0
+    until offset >= 10000 do
+      uri = URI.parse("https://api.vk.com/"+
+                      "method/photos.get?"+
+                      "owner_id=#{groupID}"+
+                      "&album_id=#{albom}"+
+                      "&offset=#{offset}"+
+                      "&count=1000")
 
-for albom in alboms
-  $offset = 0
-  until $offset >= 10000 do
-    uri = URI.parse("https://api.vk.com/method/photos.get?owner_id=#{groupID}&album_id=#{albom}&offset=#{$offset}&count=1000")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-    hashData = JSON.parse(response.body)
-    responseList = hashData["response"]
-    for record in responseList
-      if (record["user_id"].to_s).eql? personID
-        photoList.unshift("https://vk.com/photo#{groupID}_"+record["pid"].to_s)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      hashData = JSON.parse(response.body)
+      responseList = hashData["response"]
+
+      for record in responseList
+        if (record["user_id"].to_s).eql? personID
+          photoList.unshift("https://vk.com/photo#{groupID}_"+record["pid"].to_s)
+        end
       end
+      offset += 1000
     end
-    $offset += 1000
   end
-end
 
-writeHandler = File.new("Photo search result.txt", "w")
-writeHandler.puts(photoList).to_s
+  writeHandler = File.new("Search result " + "#{personID.to_s}"+".txt", "w")
+  writeHandler.puts(photoList).to_s
+  
+  puts "Done, results at: Search result " + "#{personID.to_s}"+".txt"
+else
+  puts "Usage: personID groupID {albom_id,albom_id,..}"
+end
