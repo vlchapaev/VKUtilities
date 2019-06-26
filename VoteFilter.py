68ad5485bb6414c812db42d3dd115a714fbb312c
Vladislav Chapaev <chapaev.vlad@gmail.com>
2019-06-26 22:38:50 +0300

    fix

--- VoteFilter.py
+++ VoteFilter.py
@@ -33,7 +33,7 @@ print("Теперь Id опроса:")
 idPoll = str(input()).strip()
 vApi = "5.92"
 try:
-    result = requests.get('https://api.vk.com/method/polls.getById?poll_id=' + idPoll + "&owner_id=-" + idGroup + "&access_token=" + token + "&version=" + vApi).json()
+    result = requests.get('https://api.vk.com/method/polls.getById?poll_id=' + idPoll + "&owner_id=-" + idGroup + "&access_token=" + token + "&v=" + vApi).json()
     time.sleep(0.3)
 except requests.exceptions.ReadTimeout:
     print("Метод - polls.getById, Ошибка - exceptions.ReadTimeout...")
@@ -65,7 +65,7 @@ for i in responseGetById["answers"]:
     offsetUser = 0
     while 1:
         try:
-            result = requests.get('https://api.vk.com/method/polls.getVoters?count=500&poll_id=' + idPoll + "&offset=" + str(offsetUser) + "&answer_ids=" + str(id) + "&owner_id=" + idGroup + "&access_token=" + token + "&version=" + vApi).json()
+            result = requests.get('https://api.vk.com/method/polls.getVoters?count=500&poll_id=' + idPoll + "&offset=" + str(offsetUser) + "&answer_ids=" + str(id) + "&owner_id=" + idGroup + "&access_token=" + token + "&v=" + vApi).json()
             time.sleep(0.3)
         except requests.exceptions.ReadTimeout:
             print("Метод - polls.getVoters, Ошибка - exceptions.ReadTimeout...")
@@ -76,14 +76,15 @@ for i in responseGetById["answers"]:
             input()
             continue
         responseGetVoters = checkError(result)
-        userList = responseGetVoters[0].get("users")
+        userList = responseGetVoters[0].get("users").get("items")
         if len(userList) == 1: 
             break
-        offsetUser += 500 
-        userList.pop(0)
+        offsetUser += 500
+        if len(userList) == 0:
+            break
         userIds = ",".join(map(str, userList))
         try:
-            result = requests.get('https://api.vk.com/method/groups.isMember?extended=1&group_id=' + idGroup + "&user_ids=" + userIds + "&access_token=" + token + "&version=" + vApi).json()
+            result = requests.get('https://api.vk.com/method/groups.isMember?extended=1&group_id=' + idGroup + "&user_ids=" + userIds + "&access_token=" + token + "&v=" + vApi).json()
             time.sleep(0.3)
         except requests.exceptions.ReadTimeout:
             print("Метод - polls.isMember, Ошибка - exceptions.ReadTimeout...")
@@ -91,6 +92,9 @@ for i in responseGetById["answers"]:
             print("Метод - polls.isMember, Ошибка - exceptions.ConnectTimeout...")
         responseIsMember = checkError(result)
         for i2 in responseIsMember:
+            if type(i2) is str:
+                print("пизда какая-то")
+                continue
             member = i2.get("member", "")
             user_id = i2.get("user_id", "")
             if user_id == "":
